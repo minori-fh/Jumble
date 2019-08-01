eefec808db2a77a326edce4349564932c0a8de36
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-08-01 10:53:32 -0700

    Removing renderAccount function because we just need render in Profile.js

--- client/src/pages/User/Profile/Profile.js
+++ client/src/pages/User/Profile/Profile.js
@@ -1,50 +1,61 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import Actions from "../../../utils/API";
-import "./Profile.css"
-
-import Panel from '../../../components/Panel'
-import Button from '../../../components/Button'
+import "./Profile.css";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Panel from '../../../components/Panel';
+import Button from '../../../components/Button';
+import Dashboard from '../../../components/Dashboard';
+//need to import components here
 
 class Profile extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             userAccount: false
         }
     }
-    
 
-    handlelogout(){
+
+    handlelogout() {
         Actions.handlelogout()
-        .then(data => {return data.json()})
-        .then(response=>{
-          this.setState({
-            isLoggedIn: response
-          })
+            .then(data => { return data.json() })
+            .then(response => {
+                this.setState({
+                    isLoggedIn: response
+                })
 
-          if(!response) {
-            window.location.href = "/"
-          }
-          
-        })
-        .catch(err=> console.log("err",err))
+                if (!response) {
+                    window.location.href = "/"
+                }
+
+            })
+            .catch(err => console.log("err", err))
     }
 
-    renderAccountContent(){
-        return(
-            <div>
-                <h3>Welcome to your account panel.</h3>
-                <h4>♡</h4>
-                <Button float="none" handleBtnClick={this.handlelogout.bind(this)}>logout</Button>
-            </div>
-        )
-    }
+    // renderAccountContent() {
+    //     return (
+    //         <Router>
+    //             <div>
+    //                 {/* <Sidenav /> */}
+    //                 <Switch>
+    //                     {/* we need to change :uuid and :projectid with pulled info at state */}
+    //                     {/* <Route exact path="/dashboard/:uuid/:projectid" component={Dashboard}/> */}
+    //                     {/* <Route exact path ="/dashboard/:uuid/:projectid/form" component={Form}  /> */}
+    //                 </Switch>
+    //                 <Button float="none" handleBtnClick={this.handlelogout.bind(this)}>logout</Button>
+    //             </div>
+    //         </Router>
+    //     )
+    // }
 
-    render(){
-        return(
-            <Panel>
-                {this.renderAccountContent()}
-            </Panel>
+    render() {
+        return (
+            // <Panel>
+            //     {this.renderAccountContent()}
+            // </Panel>
+            <Dashboard>
+                <h1>Hi</h1>
+            </Dashboard>
         )
     }
 }
