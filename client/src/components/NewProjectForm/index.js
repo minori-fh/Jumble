e57e808c52d4f2ca2322a6198928707ea1c57c19
Adam <lehreradam@yahoo.com>
2019-08-01 10:50:28 -0700

    setting up for for new projects

---
+++ client/src/components/NewProjectForm/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function NewProjectForm(){
+    return(
+        <div>
+            <input></input>
+            <input></input>
+            <input></input>
+            <input></input>
+            <input></input>
+        </div>
+    )
+}
+
+export default NewProjectForm
