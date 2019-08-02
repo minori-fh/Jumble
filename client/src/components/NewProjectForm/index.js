b9659f8b4b1e06de82b98df94858f35bbe484829
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-08-02 11:36:46 -0700

    Adding formRender function in order to use if else statements to determine which form to render in index.js of NewProjectForm

--- client/src/components/NewProjectForm/index.js
+++ client/src/components/NewProjectForm/index.js
@@ -5,7 +5,8 @@ class NewProjectForm extends Component {
     
     state = {
         title:"",
-        budget:""
+        budget:"",
+        task: ""
     }
     
     
@@ -21,32 +22,71 @@ class NewProjectForm extends Component {
     }
 
     
-    render(){
-    return (
+    formRender() {
+        if (this.state.title === "") {
+            return (
+                <div>
+                    <form id="styling">
+                        <p id="typedTitle">{this.state.title}</p>
+                        <p id="typedBudget">{this.state.budget}</p>
+                        <input
+                            id="inputName"
+                            type="text"
+                            value={this.state.title}
+                            placeholder="Project Name"
+                            onChange={this.handelInputChange}
+                            name="title"
+                        />
+                        <input
+                            id="inputBudget"
+                            type="text"
+                            placeholder="Project Budget"
+                            onChange={this.handelInputChange}
+                            value={this.state.budget}
+                            name="budget"
+                        />
+                        <button id="submitNewProject"> Submit </button>
+                    </form>
+                </div>
+            )
+        }
+        else if (this.state.budget === "" && this.state.task === "") {
+            <BudgetForm />
+        }
+        else {
+            <AssigneeForm />
+        }
+    }
+
+    render() {
+        // return (
+        //     <div>
+        //         <form id="styling">
+        //             <p id="typedTitle">{this.state.title}</p>
+        //             <p id="typedBudget">{this.state.budget}</p>
+        //             <input
+        //                 id="inputName"
+        //                 type="text"
+        //                 value={this.state.title}
+        //                 placeholder="Project Name"
+        //                 onChange={this.handelInputChange}
+        //                 name="title"
+        //             />
+        //             <input
+        //                 id="inputBudget"
+        //                 type="text"
+        //                 placeholder="Project Budget"
+        //                 onChange={this.handelInputChange}
+        //                 value={this.state.budget}
+        //                 name="budget"
+        //             />
+        //             <button id="submitNewProject"> Submit </button>
+        //         </form>
+        //     </div>
+        // )
         <div>
-            <form id="styling">
-            <p id="typedTitle">{this.state.title}</p>
-            <p id="typedBudget">{this.state.budget}</p>
-                <input
-                id="inputName"
-                type ="text"
-                value={this.state.title}
-                placeholder="Project Name"
-                onChange={this.handelInputChange}
-                name="title"
-                /> 
-                <input
-                id="inputBudget"
-                type ="text"
-                placeholder="Project Budget"
-                onChange={this.handelInputChange}
-                value={this.state.budget}
-                name="budget"
-                />
-                <button id="submitNewProject"> Submit </button>
-            </form>
+            {this.formRender()}
         </div>
-    )
     }
 }
 
