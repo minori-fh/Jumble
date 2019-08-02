696527ee06e8b3cbfb3398acf20cd8e3df579522
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-08-02 16:09:25 -0700

    Adding dynamically rendering task form to index.js of NewProjectForm with new component Task Form

--- client/src/components/NewProjectForm/index.js
+++ client/src/components/NewProjectForm/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import "./style.css";
 import API from "../../utils/API-project";
+import TaskForm from "../TaskForm"
 
 class NewProjectForm extends Component {
 
     state = {
-        title: ""
+        title: "",
+        savedTitle: false,
+        budget: "",
+        tasks:[{
+            name: "",
+            assignees: []
+        }]
     }
 
     handleInputChange = event => {
@@ -18,6 +25,10 @@ class NewProjectForm extends Component {
 
     saveProject = () => {
 
+        this.setState({
+            savedTitle: true
+        });
+
         const body = {
             name: this.state.title
         }
@@ -25,27 +36,82 @@ class NewProjectForm extends Component {
         API.createProject(body)
             .then(res => {
                 console.log("LOOK HERE=======================================" + res)
-        })
-        .catch(err => console.log(err.message));
+            })
+            .catch(err => console.log(err.message));
     }
 
+    addTask = event => {
+        event.preventDefault();
+        const newTask = this.state.tasks;
+        newTask.push({name: "", assignees: [""]});
+        console.log(newTask);
+        this.setState({
+            tasks: newTask
+        });
+    }
+
+    formRender() {
+        if (this.state.savedTitle === false) {
+            return (
+                <div>
+                    <form id="styling">
+                        <p id="typedTitle">{this.state.title}</p>
+                        <input
+                            id="inputName"
+                            type="text"
+                            value={this.state.title}
+                            placeholder="Project Name"
+                            onChange={this.handleInputChange}
+                            name="title"
+                        />
+                        <button id="submitNewProject" onClick={this.saveProject}> Next </button>
+                    </form>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <form id="styling">
+                        <p id="typedBudget">{this.state.budget}</p>
+                        <input
+                            id="inputBudget"
+                            type="text"
+                            value={this.state.budget}
+                            placeholder="Budget"
+                            onChange={this.handleInputChange}
+                            name="budget"
+                        />
+                        {this.state.tasks.map((task, i) => {
+                            return (<TaskForm key={i}/>);
+                        })}
+                        <button id="addTask" onClick={this.addTask}>+Task</button>
+                    </form>
+                </div>
+            )
+
+        }
+    }
 
     render() {
         return (
             <div>
-                <form id="styling">
-                    <p id="typedTitle">{this.state.title}</p>
-                    <input
-                        id="inputName"
-                        type="text"
-                        value={this.state.title}
-                        placeholder="Project Name"
-                        onChange={this.handleInputChange}
-                        name="title"
-                    />
-                    <button id="submitNewProject" onClick={this.saveProject}> Submit </button>
-                </form>
+                {this.formRender()}
             </div>
+            // <div>
+            //     <form id="styling">
+            //         <p id="typedTitle">{this.state.title}</p>
+            //         <input
+            //             id="inputName"
+            //             type="text"
+            //             value={this.state.title}
+            //             placeholder="Project Name"
+            //             onChange={this.handleInputChange}
+            //             name="title"
+            //         />
+            //         <button id="submitNewProject" onClick={this.saveProject}> Submit </button>
+            //     </form>
+            // </div>
         )
     }
 }
