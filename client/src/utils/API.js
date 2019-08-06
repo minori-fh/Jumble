ebf937a61871a69c973aa5c4b424dd4e8f21c44e
Minori Hashimoto <minori.fh@gmail.com>
2019-08-05 20:23:48 -0700

    Add react app to github

---
+++ client/src/utils/API.js
@@ -0,0 +1,38 @@
+export default {
+	checkAuth: function(){
+		return fetch("https://jumbletron.herokuapp.com//auth", {
+      method: 'GET',
+      credentials: 'include',
+      mode: 'cors'
+    });
+	},
+	handleLogin: function(userData){
+		return fetch("https://jumbletron.herokuapp.com//signin", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify(userData),
+        credentials: 'include',
+        mode: 'cors'
+      });
+	},
+	handleSignup: function(userData){
+		return fetch("https://jumbletron.herokuapp.com//signup", {
+      method: 'POST',
+      headers: {
+          "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify(userData),
+      credentials: 'include',
+      mode: 'cors'
+    });
+	},
+	handlelogout: function(){
+		return fetch("https://jumbletron.herokuapp.com//logout", {
+      method: 'GET',
+      credentials: 'include',
+      mode: 'cors'
+    });
+	}	
+}
