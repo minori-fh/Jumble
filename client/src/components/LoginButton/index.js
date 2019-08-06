68bf4bd9160a7497e6bcbeefdd00d39b384ccd64
Minori Hashimoto <minori.fh@gmail.com>
2019-08-05 20:06:22 -0700

    Just the server

--- client/src/components/LoginButton/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-const LoginButton = (props) => (
-    <button className={`btn ${props.float}`} onClick={props.handleBtnClick}> {props.children} </button> 
-);
-
-export default LoginButton;
--- client/src/components/LogoutButton/LogoutButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-const LogoutButton = (props) => (
-    <button className='logout' onClick={props.logout}>Logout</button> 
-);
-
-export default LogoutButton;
