801c786940e8aebd7e62b037822ca95a6c564861
Jake Dudum <jakedudum@gmail.com>
2019-08-05 19:28:03 -0700

    Filled out components to load project data when clicked on

--- client/src/components/Tasks/index.js
+++ client/src/components/Tasks/index.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { Component } from 'react';
+import TaskAPI from '../../utils/API-task';
 
-function Tasks(props) {
-    return (
-        <div>
-            <p>{props.tasks}</p>
-        </div>
-    );
+class Tasks extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: []
+        }
+    }
+
+    componentDidMount() {
+        TaskAPI.getTasks(this.props.projectID).then(res => {
+            console.log(res.data),
+                this.setState({
+                    tasks: res.data
+                })
+        })
+            .catch(err => console.log(err.message));
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.tasks.map((task,i) => (
+                    <p key={i}>{task.task}</p>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default Tasks;
