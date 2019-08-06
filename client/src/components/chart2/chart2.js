5f69e1c3f35e76bb22224fe7019a97e7910294fc
Minori Hashimoto <minori.fh@gmail.com>
2019-08-05 18:24:21 -0700

    render chart2 (linegraph) onto dashboard component

--- client/src/components/chart2/chart2.js
+++ client/src/components/chart2/chart2.js
@@ -1,58 +1,58 @@
-// import React, {Component} from 'react';
-// var Chart = require("chart.js")
+import React, {Component} from 'react';
+var Chart = require("chart.js")
 
-// class Chart2 extends Component {
+class Chart2 extends Component {
 
-//     constructor(props){
-//         super(props);
-//         this.chart2Ref = React.createRef(); 
-//     }
+    constructor(props){
+        super(props);
+        this.chart2Ref = React.createRef(); 
+    }
 
-//     componentDidMount() {
-//         this.chart2 = new Chart(this.chart2Ref.current, {
-//             type: 'line',
-//             options: {
-//               scales: {
-//                 xAxes: [
-//                   {
-//                     type: 'time',
-//                     time: {
-//                       unit: 'day'
-//                     }
-//                   }
-//                 ],
-//                 yAxes: [
-//                   {
-//                     ticks: {
-//                       min: 0,
-//                       max: 100
-//                     }
-//                   }
-//                 ]
-//               }
-//             },
-//             data: {
-//               labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-//               datasets: [{
-//                 label: ["Progress (%)"],
-//                 data: [10, 25, 50, 46, 60],
-//                 fill: 'none',
-//                 backgroundColor: "#3E517A",
-//                 pointRadius: 2,
-//                 borderColor: "#3E517A",
-//                 borderWidth: 1,
-//                 lineTension: 0
-//               }]
-//             }
-//           });
-//       }
+    componentDidMount() {
+        this.chart2 = new Chart(this.chart2Ref.current, {
+            type: 'line',
+            options: {
+              scales: {
+                xAxes: [
+                  {
+                    type: 'time',
+                    time: {
+                      unit: 'day'
+                    }
+                  }
+                ],
+                yAxes: [
+                  {
+                    ticks: {
+                      min: 0,
+                      max: 100
+                    }
+                  }
+                ]
+              }
+            },
+            data: {
+              labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+              datasets: [{
+                label: ["Progress (%)"],
+                data: [10, 25, 50, 46, 60],
+                fill: 'none',
+                backgroundColor: "#3E517A",
+                pointRadius: 2,
+                borderColor: "#3E517A",
+                borderWidth: 1,
+                lineTension: 0
+              }]
+            }
+          });
+      }
 
-//     render(){
-//         return(
-//             <canvas className = 'chart' ref={this.chart2Ref}/>
-//         )
-//     };
+    render(){
+        return(
+            <canvas className = 'chart' ref={this.chart2Ref}/>
+        )
+    };
 
-// };
+};
 
-// export default Chart2;
+export default Chart2;
